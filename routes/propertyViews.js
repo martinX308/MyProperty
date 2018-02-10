6a70aacd255b841e1f51124a6a84bcc687c75f32
Martin <mar.gardemann@gmail.com>
2018-02-10 11:47:08 +0100

    started on post create

--- routes/propertyViews.js
+++ routes/propertyViews.js
@@ -1,5 +1,9 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+
+// link property models
+const Property = require('../models/property');
+const User = require('../models/user');
 
 router.get('/my-properties', (req, res, next) => {
   res.render('properties/myproperties');
@@ -10,7 +14,23 @@ router.get('/create', (req, res, next) => {
 });
 
 router.post('/create', (req, res, next) => {
+  const username = request.session.username;
+  const name = req.body.name;
+  const street = req.body.street;
+  const nr = req.body.nr;
+  const zip = req.body.zip;
+  const city = req.body.city;
+  const country = req.body.country;
 
+  if (name === '' || street === '' || nr === '' || zip === '' || city === '' || country === '') {
+    res.render('properties/create', { message: 'Indicate all fields' });
+  }
+
+  User.findOne({ username }, 'username', (err, user, next) => {
+    if (err) {
+      return next(err);
+    }
+  });
 });
 
 module.exports = router;
