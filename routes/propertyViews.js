93fc5c053d7f58ddebe04171dc65e169c2da9d24
Martin <mar.gardemann@gmail.com>
2018-02-12 14:57:07 +0100

    v1 viewproperty controller

--- routes/propertyViews.js
+++ routes/propertyViews.js
@@ -53,4 +53,27 @@ router.post('/create', (req, res, next) => {
   });
 });
 
+router.get('/properties/view/:id', (req, res, next) => {
+  const propId = req.params._id;
+  Property.findById(propId, 'accountingbook owner', (err, property, next) => {
+    if (err) {
+      return next(err);
+    }
+    if (req.user !== property.owner) {
+      res.redirect('/properties/my-properties');
+      return;
+    }
+
+    const aggregationMonth = property.accountingbook.map(element =>
+      element.date.map(x => [x.getMonth(), x.getYear()]));
+
+    const aggregationType = aggregationMonth.reduce((acc, transaction) => {
+      let selectedYear = transaction.date[1];
+      acc[selectedYear][transaction.type][transaction.name] += transaction.value;
+      return acc;
+    }, {});
+
+    res.render('properties/viewproperty', {transactions: aggregationType});
+  });
+});
 module.exports = router;
