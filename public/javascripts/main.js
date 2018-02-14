298c2b9b316edaa0b2a6c082ff19eea4b27ac842
Santi Ramos <santi92.network@gmail.com>
2018-02-14 16:17:07 +0100

    ignore this commit

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -49,21 +49,5 @@ function main () {
 }
 
 
-// Google Maps Seetings
-
-var ironhackBCN = {
-  lat: 41.3977381,
-  lng: 2.190471916
-};
-
-var map = new google.maps.Map(
-  document.getElementById('map'),
-  {
-    zoom: 15,
-    center: ironhackBCN
-  }
-);
-
-
 
 window.onload = main;
