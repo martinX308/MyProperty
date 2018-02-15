0364534561c67db2e5e12b261c5e723215c58ea8
Santi Ramos <santi92.network@gmail.com>
2018-02-15 13:28:59 +0100

    Autocomplete Google Maps API fill the rest of form inputs

--- public/javascripts/autocomplete-address.js
+++ public/javascripts/autocomplete-address.js
@@ -13,21 +13,35 @@ function startAutocomplete() {
           var place = autocomplete.getPlace();
           var lat = place.geometry.location.lat();
           var lng = place.geometry.location.lng();
-          var placeId = place.place_id;
-          // to set city name, using the locality param
-          var componentForm = {
-            locality: 'short_name',
-          };
+          // var placeId = place.place_id;
+
           for (var i = 0; i < place.address_components.length; i++) {
             var addressType = place.address_components[i].types[0];
-            if (componentForm[addressType]) {
-              var val = place.address_components[i][componentForm[addressType]];
-              document.getElementById("city").value = val;
+            console.log(addressType);
+            if (addressType === 'locality') {
+              const val = place.address_components[i].long_name;
+              document.getElementById("cityCreate").value = val;
+            }
+            if (addressType === "postal_code") {
+              const val = place.address_components[i].long_name;
+              document.getElementById("zipCreate").value = val;
+            }
+            if (addressType === "street_number") {
+              const val = place.address_components[i].long_name;
+              document.getElementById("streetnrCreate").value = val;
+            }
+            if (addressType === "route") {
+              const val = place.address_components[i].long_name;
+              document.getElementById("streetCreate").value = val;
+            }
+            if (addressType === "country") {
+              const val = place.address_components[i].long_name;
+              document.getElementById("countryCreate").value = val;
             }
           }
           document.getElementById("latitude").value = lat;
           document.getElementById("longitude").value = lng;
-          document.getElementById("location_id").value = placeId;
+          // document.getElementById("location_id").value = placeId;
         });
 }
 
